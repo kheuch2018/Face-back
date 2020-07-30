b220eb5564fca2f8c4f8b28b20425ea77c20e064
kheuch2020 <cheikh@mobility-sn.com>
2020-07-30 10:36:39 +0000

    updating backend

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const swaggerOptions = {
       contact: {
         name: "Cheikh Seck"
       },
-      servers: ["http://localhost: 5000"]
+      servers: ["http://localhost:5000"]
     }
   },
   apis: ["index.js"]
@@ -42,33 +42,82 @@ app.use(fileupload());
 app.use(cors());
 
 app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs))
+let s3bucket = new AWS.S3({
+  accessKeyId: IAM_USER_KEY,
+  secretAccessKey: IAM_USER_SECRET,
+  Bucket: BUCKET_NAME,
+});
+var params = {
+  Bucket: BUCKET_NAME,
+  Key: "",
+  Body: "",
+};
 
-
-function uploadToS3(file, fileName, res,type) {
-  let s3bucket = new AWS.S3({
-    accessKeyId: IAM_USER_KEY,
-    secretAccessKey: IAM_USER_SECRET,
-    Bucket: BUCKET_NAME,
-  });
+function uploadToS3(userImage, idImage, fileName, res) {
   s3bucket.createBucket(function () {
-    var params = {
-      Bucket: BUCKET_NAME,
-      Key: fileName,
-      Body: file.data,
-    };
+    params.Key=fileName
+    params.Body=userImage.data
     s3bucket.upload(params, function (err, data) {
       if (err) {
-        console.log("error in callback");
-        console.log(err);
-        res.status(400).send({ err });
+        res.status(400).send({ err, code: 2 });
       }
-      console.log("success");
-      console.log(data);
-      if(type!=="compare") return  res.send({ success: data });
-      compareFaces(res,fileName)
+     console.log("upload 1 succeed")
+
       
-    });
-  });
+      detectFaces(res,fileName,idImage)
+      
+      // if(type!=="compare") return res.send({ success: "Face correct" });
+      // compareFaces(res,fileName)
+      
+    })
+  })
+}
+
+let detectFaces = (res, fn,idImage) => {
+  const client = new AWS.Rekognition();
+  const params2 = {
+    Image: {
+      S3Object: {
+        Bucket: "kalpay-faces",
+        Name: fn
+      },
+    },
+    Attributes: ['ALL']
+  }
+  
+  client.detectFaces(params2,(err,response) => {
+    if(err) return res.status(400).send({err: "No face detected", code: 3 })
+    // return res.send(response)
+    if(response.FaceDetails && 
+      response.FaceDetails[0].Smile.Value && response.FaceDetails[0].Smile.Confidence > 90 && 
+      !response.FaceDetails[0].EyesOpen.Value && response.FaceDetails[0].EyesOpen.Confidence > 90 
+      ) {
+     console.log("Face is detected")
+        
+        s3bucket.createBucket(function () {
+           params.Key="compare"+fn
+           params.Body=idImage.data
+          s3bucket.upload(params, function (err, data) {
+            if (err) {
+              console.log("error in callback");
+              console.log(err);
+              res.status(400).send({ err, code: 4 });
+            }
+            console.log("Upload 2 succeed")
+            compareFaces(res,fn)
+            
+            // if(type!=="compare") return res.send({ success: "Face correct" });
+            
+          })
+        })
+
+
+        return
+      }
+
+    else
+    return res.status(400).send({err: "Invalid Face", code: 5 })
+  })
 }
 
 let compareFaces = (res,fileName) => {
@@ -77,7 +126,7 @@ let compareFaces = (res,fileName) => {
     SourceImage: {
       S3Object: {
         Bucket: BUCKET_NAME,
-        Name: fileName.split("-")[1],
+        Name: "compare"+fileName,
       },
     },
     TargetImage: {
@@ -92,8 +141,9 @@ let compareFaces = (res,fileName) => {
   client.compareFaces(Myparams, (err, response) => {
     if (err) {
       console.log(err, err.stack); // an error occurred
-      res.status(400).send({ err });
+      res.status(400).send({ err, code: 6 });
     } else {
+      console.log("Faces compared");
       response.FaceMatches.forEach((data) => {
         let position = data.Face.BoundingBox;
         let similarity = data.Similarity;
@@ -104,57 +154,11 @@ let compareFaces = (res,fileName) => {
       }); // for response.faceDetails
 
       response.UnmatchedFaces.forEach((data) => {
-        res.status(404).send({err: "Faces not matching" });
+        res.status(404).send({err: "Faces not matching", code: 7 });
       })
-    } // if
+    } 
   });
 }
-/**
- * @swagger
- * /api/upload:
- *  post:
- *    description: Used to upload user image when signing up
- *    parameters:
- *      - name: image
- *        description: Image to upload
- *        in: body
- *        required: true
- *      - name: idUser
- *        description: Id of the user
- *        in: body
- *        required: true
- *    responses:
- *      '200': 
- *        description: Image uploaded successfuly
- *      '400':
- *        description: Invalid parameters
- */
-app.post("/api/upload", function (req, res, next) {
-  // This grabs the additional parameters so in this case passing in
-console.log("heho")
-  const { idUser: fileName } = req.body;
-  if (!fileName) return res.status(400).send({ err: "Paramètres invalides" });
-  var busboy = new Busboy({ headers: req.headers });
-
-  // The file upload has completed
-  busboy.on("finish", function () {
-    console.log("Upload finished");
-
-    // Grabs your file object from the request.
-    if (!req.files || !req.files.image)
-      return res.status(400).send({ err: "Paramètres invalides" });
-    const { image: file } = req.files;
-
-    console.log(file);
-    
-    // Begins the upload to the AWS S3
-    uploadToS3(file, fileName, res);
-  });
-
-  req.pipe(busboy);
-});
-
-
 
 
 /**
@@ -163,8 +167,12 @@ console.log("heho")
  *  post:
  *    description: Used to identify the user
  *    parameters:
- *      - name: image
- *        description: Image to upload for identification
+ *      - name: idImage
+ *        description: Image of the CNI/Passport or other id document
+ *        in: body
+ *        required: true
+ *      - name: userImage
+ *        description: Image of the user with eyes closed and smiling
  *        in: body
  *        required: true
  *      - name: idUser
@@ -181,7 +189,7 @@ console.log("heho")
  */
 app.post("/api/identify", (req, res) => {
   const { idUser:fileName } = req.body;
-  if(!fileName) return res.status(400).send({ err: "Paramètres invalides -2" });
+  if(!fileName) return res.status(400).send({ err: "Paramètres invalides ",code: 0 });
   var busboy = new Busboy({ headers: req.headers });
     
   // The file upload has completed
@@ -189,14 +197,13 @@ app.post("/api/identify", (req, res) => {
     console.log("Upload finished");
     
     // Grabs your file object from the request.
-    if (!req.files || !req.files.image)
-      return res.status(400).send({ err: "Paramètres invalides -1" });
-    const { image: file } = req.files;
+    if (!req.files || !req.files.userImage || !req.files.idImage)
+      return res.status(400).send({ err: "Paramètres invalides ", code: 1 });
+    const { userImage,idImage } = req.files;
 
-    console.log(file);
-    let fileToCompare = "compare-"+fileName
     // Begins the upload to the AWS S3
-    uploadToS3(file, fileToCompare,res,"compare");
+
+    uploadToS3(userImage,idImage, fileName,res);
   });
 
 
