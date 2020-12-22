de5bc3a2b16e65d9e9fbea3de9108ce9b2aeb0e0
kheuch2018 <42771777+kheuch2018@users.noreply.github.com>
2020-12-22 22:01:44 +0000

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 module.exports={
     BUCKET_NAME: "kalpay-faces",
-    IAM_USER_KEY: "AKIAQ7YRTJKBIKVYEZYW",
+    IAM_USER_KEY: "AKIAQ7YRTJKBPECRB2MC",
     IAM_USER_SECRET: "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",
     AWS_REGION: "us-east-2"
 }
